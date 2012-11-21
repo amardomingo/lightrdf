5cfd69be6c9cf83bb68210ceebc8c3d37c498c26
amardomingo <mardoalmar@gmail.com>
2012-11-21 19:31:14 +0100

    Added omr settings

--- lib/lightrdf/node.rb
+++ lib/lightrdf/node.rb
@@ -19,36 +19,39 @@ module RDF
       end
     end
   end
-
+  
   class Node < Hash
     include Parser
+    @classes = {}
+    def self.classes; @classes; end
+    
     attr_accessor :graph, :id
 
     def initialize id=nil, graph=nil
-      @id = ID::parse(id)
+      @id = ID(id)
       @graph = graph || Graph.new
-      @graph << self
+      @graph[@id] = self
     end
 
     def method_missing method, *args
-      QURI.ns[method] ? NsProxy.new(method, self) : super
+      ID.ns[method] ? NsProxy.new(method, self) : super
     end
     def inspect; "<#{self.class} #{id} #{super}>"; end
-    def to_s; id.to_s; end
+    def to_s;   id.to_s; end
+    def to_sym; id;      end
 
     def [] name
-      self[Node(name)] = [] if super(Node(name)).nil?
-      super(Node(name)).map! {|n| n.is_a?(Node) ? @graph[n] : n}
+      ( super(ID(name)) || [] ).clone
     end
     def []= name, values
-      super(Node(name), [values].flatten.map { |node| node.is_a?(Node) ? @graph[node] : node })
+      super(ID(name), [values].flatten.map { |node| node.is_a?(Node) ? @graph[node] : node })
     end
 
-    def == b
-      eql? b
+    def == node
+      eql? node
     end
-    def eql? b
-      b.is_a?(Node) and self.id == b.id
+    def eql? node
+      self.class == node.class and self.id == node.id
     end
     def hash # Hack for Ruby 1.8.6
       id.hash ^ self.class.hash
@@ -59,28 +62,58 @@ module RDF
     end
 
     def triples
-      triples = []; each { |k, v| v.each { |o| triples << [self, Node(k), o] } }
+      triples = []; each { |p, v| v.each { |o| triples << [id, p, o.is_a?(Node) ? o.id : o] } }
+      triples
+    end
+    
+    def all_triples done=[]
+      return [] if done.include?(self)
+      done << self
+      triples = self.triples
+      triples.map { |s,p,o| [s,o] }.flatten.uniq.each { |node| triples += graph[node].all_triples(done) if node.is_a?(Symbol) }
       triples
     end
     
     def merge node
-      new_node = clone
+      new_node = Node(id)
       (self.keys + node.keys).uniq.each do |k|
         new_node[k] = (node[k] + self[k]).uniq
       end
       new_node
     end
-
-    def clone
-      Node.new self
+    
+    def merge! node
+      (self.keys + node.keys).uniq.each do |k|
+        self[k] = (node[k] + self[k]).uniq
+      end
+      self
     end
 
+    def rename new_id=nil
+      clone.rename! new_id
+    end
+    
+    def rename! new_id=nil
+      self.graph.delete id
+      self.id = ID(new_id)
+      self.graph[id] = self
+      self
+    end
+    
+    def clone
+      self.graph.clone[id]
+    end
+    
     def bnode?
-      id.is_a?(BNodeID)
-    end    
+      ID.bnode?(id)
+    end
+    
+    def proxy type=nil
+      graph.node(self, type)
+    end
   end
 end
 
 def Node id, graph=nil
-  graph.nil? ? RDF::Node.new(id, graph) : graph[id]
+  graph ? graph[id] : RDF::Node.new(id)
 end
